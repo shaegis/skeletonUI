66da55a49935804eb7b83abc9b09038c9f8e0b40
shaegis <shaegis@gmail.com>
2024-11-15 17:09:52 +0900

    adding AppRail. SSR related error.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,14 @@
 <script lang="ts">
 	import '../app.postcss';
 	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
+    import { AppRail, AppRailTitle, AppRailAnchor } from '@skeletonlabs/skeleton';
+
+    import { Home } from 'lucide-svelte';
+    import { NotePen } from 'lucide-svelte';
+    import { Ruler } from 'lucide-svelte';
+    import { Settings } from 'lucide-svelte';
+
+    let currentTitle: number = 0;
 </script>
 
 <!-- App Shell -->
@@ -38,6 +46,32 @@
 				</a>
 			</svelte:fragment>
 		</AppBar>
+        <AppRail>
+            <svelte:fragment slot="lead">
+                <AppRailAnchor href="/" >
+                    <Home class="w-6 h-6 text-gray-500" />
+                </AppRailAnchor>
+            </svelte:fragment>
+            <!-- --- -->
+            <AppRailTitle bind:group={currentTitle} name="Progress" value={0} title="Progress">
+                <svelte:fragment slot="lead">
+                    <NotePen class="w-6 h-6 text-gray-500" />
+                    <span>Progress</span>
+                </svelte:fragment>
+            </AppRailTitle>
+            <AppRailTitle bind:group={currentTitle} name="Scale" value={0} title="Scale">
+                <svelte:fragment slot="lead">
+                    <Ruler class="w-6 h-6 text-gray-500" />
+                    <span>Scale</span>
+                </svelte:fragment>
+            </AppRailTitle>
+            <AppRailTitle bind:group={currentTitle} name="Settings" value={0} title="Settings">
+                <svelte:fragment slot="lead">
+                    <Settings class="w-6 h-6 text-gray-500" />
+                    <span>Scale</span>
+                </svelte:fragment>
+            </AppRailTitle>
+        </AppRail>
 	</svelte:fragment>
 	<!-- Page Route Content -->
 	<slot />
