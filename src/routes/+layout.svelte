cba1fd4fa9d487e3aedb792ba02eb547b8587a29
shaegis <shaegis@gmail.com>
2024-11-20 18:34:16 +0900

    Remove Tailwind code, then Works good. Problem with showing contents.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -15,74 +15,69 @@
     }
 </script>
 
-<!-- App Shell -->
-<AppShell>
-	<svelte:fragment slot="header">
-		<!-- App Bar -->
-		<AppBar>
-			<svelte:fragment slot="lead">
-                <div class="flex items-center gap-2">
-                    {#if showBackButton}
-                        <button 
-                            type="button"
-                            class="variant-soft-primary flex items-center justify-center w-10 h-10 sm:w-12 sm:h-12 lg:w-14 lg:h-14 cursor-pointer"
-                            on:click={() => window.history.back()}
-                            on:keydown={(e) => { if (e.key === 'Enter' || e.key === ' ') window.history.back(); }}
-                            aria-label="Go Back"
-                        >
-                            <!-- MoveLeft 아이콘 -->
-                            <MoveLeft class="w-6 h-6 sm:w-8 sm:h-8 lg:w-10 lg:h-10" />
-                        </button>
-                    {/if}
-                    <span class="text-primary font-bold text-lg sm:text-xl uppercase">{title}</span>
-                </div>
-			</svelte:fragment>
-			<svelte:fragment slot="trail">
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://discord.gg/EXqV7W8MtY"
-					target="_blank"
-					rel="noreferrer"
-				>
-					Discord
-				</a>
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://twitter.com/SkeletonUI"
-					target="_blank"
-					rel="noreferrer"
-				>
-					Twitter
-				</a>
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://github.com/skeletonlabs/skeleton"
-					target="_blank"
-					rel="noreferrer"
-				>
-					GitHub
-				</a>
-			</svelte:fragment>
-		</AppBar>
-        <AppRail>
-            <svelte:fragment slot="lead">
-                <AppRailAnchor href="/" class="variant-soft-primary flex items-center justify-center text-center w-12 h-12 sm:w-14 sm:h-14 lg:w-16 lg:h-16">
-                        <Home class="w-6 h-6 sm:w-8 sm:h-8 lg:w-10 lg:h-10" />
-                </AppRailAnchor>
-					
-                <!-- --- -->
-                <AppRailAnchor href="/progress" class="variant-soft-primary flex items-center justify-center w-12 h-12 sm:w-14 sm:h-14 lg:w-16 lg:h-16">
-                        <Progress class="w-6 h-6 sm:w-8 sm:h-8 lg:w-10 lg:h-10" />
-                </AppRailAnchor>
-                <AppRailAnchor href="/scale" class="variant-soft-primary flex items-center justify-center w-12 h-12 sm:w-14 sm:h-14 lg:w-16 lg:h-16">
-                        <Scale class="w-6 h-6 sm:w-8 sm:h-8 lg:w-10 lg:h-10" />
-                </AppRailAnchor>
-                <AppRailAnchor href="/settings" class="variant-soft-primary flex items-center justify-center w-12 h-12 sm:w-14 sm:h-14 lg:w-16 lg:h-16">
-                        <Settings class="w-6 h-6 sm:w-8 sm:h-8 lg:w-10 lg:h-10" />
-                </AppRailAnchor>
-            </svelte:fragment>
-        </AppRail>
-	</svelte:fragment>
-	<!-- Page Route Content -->
-	<slot />
-</AppShell>
+<header>
+    <AppBar>
+        <svelte:fragment slot="lead">
+            {#if showBackButton}
+                <button 
+                    type="button"
+                    on:click={() => window.history.back()}
+                    on:keydown={(e) => { if (e.key === 'Enter' || e.key === ' ') window.history.back(); }}
+                    aria-label="Go Back"
+                >
+                    <!-- MoveLeft 아이콘 -->
+                    <MoveLeft />
+                </button>
+            {/if}
+            <span>{title}</span>
+        </svelte:fragment>
+        <svelte:fragment slot="trail">
+            <a
+                class="btn btn-sm variant-ghost-surface"
+                href="https://discord.gg/EXqV7W8MtY"
+                target="_blank"
+                rel="noreferrer"
+            >
+                Discord
+            </a>
+            <a
+                class="btn btn-sm variant-ghost-surface"
+                href="https://twitter.com/SkeletonUI"
+                target="_blank"
+                rel="noreferrer"
+            >
+                Twitter
+            </a>
+            <a
+                class="btn btn-sm variant-ghost-surface"
+                href="https://github.com/skeletonlabs/skeleton"
+                target="_blank"
+                rel="noreferrer"
+            >
+                GitHub
+            </a>
+        </svelte:fragment>
+    </AppBar>
+</header>
+
+<AppRail>
+    <svelte:fragment slot="lead">
+        <AppRailAnchor href="/">
+                <Home />
+        </AppRailAnchor>
+        <AppRailAnchor href="/progress">
+                <Progress />
+        </AppRailAnchor>
+        <AppRailAnchor href="/scale">
+                <Scale />
+        </AppRailAnchor>
+        <AppRailAnchor href="/settings"> 
+                <Settings />
+        </AppRailAnchor>
+    </svelte:fragment>
+</AppRail>
+
+<!-- Page Route Content -->
+<main>
+    <slot />
+</main>
